58060f4feca02b74725ec1c30ef2a07b5436cb32
Adrian Pavelescu <adrian@acpgr.com>
2018-07-07 21:47:14 -0400

    split webmvcconfig to own file

--- src/main/java/com/notes/Application.java
+++ src/main/java/com/notes/Application.java
@@ -2,31 +2,12 @@ package com.notes;
 
 import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages={"com.notes"})
 @EnableEmailTools
-@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
-
-    @Bean
-    WebMvcConfigurer configurer () {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addResourceHandlers (ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/assets/note/images/**").
-                    addResourceLocations("file:./assets/uploads/");
-            }
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/notes/config/WebMvcConfiguration.java
+++ src/main/java/com/notes/config/WebMvcConfiguration.java
@@ -1,7 +1,22 @@
 package com.notes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebMvcConfiguration {
+
+    @Bean
+    WebMvcConfigurer configurer () {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers (ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/assets/note/images/**").
+                    addResourceLocations("file:./assets/uploads/");
+            }
+        };
+    }
 }
