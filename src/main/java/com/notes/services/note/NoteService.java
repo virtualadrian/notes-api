35484cfa4e5c03749e5213e8402a88ec0b08addc
Adrian Pavelescu <adrian@acpgr.com>
2018-08-18 12:30:09 -0400

    search notes by title and body, paaged for current user

--- src/main/java/com/notes/services/note/NoteService.java
+++ src/main/java/com/notes/services/note/NoteService.java
@@ -2,35 +2,23 @@ package com.notes.services.note;
 
 import com.notes.core.BaseCrudService;
 import com.notes.security.util.SecurityUtil;
-import com.notes.services.account.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.imageio.ImageIO;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
 @Service
 public class NoteService extends BaseCrudService<NoteModel, NoteEntity, Long> {
 
+    private final NoteRepository noteRepository;
+
     @Autowired
-    private AccountService accountService;
-
-    @Value("${spring.assets.static.path}")
-    private String uploadPath;
-
-    public NoteService() {
+    public NoteService(NoteRepository noteRepository) {
         super(NoteModel.class, NoteEntity.class);
+        this.noteRepository = noteRepository;
     }
 
     NoteModel createForCurrentUser(NoteModel newNote) {
@@ -48,64 +36,13 @@ public class NoteService extends BaseCrudService<NoteModel, NoteEntity, Long> {
         return this.findall(search, page, pageSize);
     }
 
-    boolean isImage(MultipartFile file) {
-        try (InputStream input = file.getInputStream()) {
-            try {
-                ImageIO.read(input).toString();
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
-        } catch (IOException ioe) {
-            return false;
-        }
-    }
+    Page<NoteModel> findAllForCurrentUserByTerm(String term, int page, int pageSize) {
+        Pageable pageRequest = new PageRequest(page, pageSize);
+        Iterable<NoteEntity> noteEntityList = this.noteRepository
+            .findAllByAccountIdAndNoteBodyContainsOrNoteTitleContains(
+                SecurityUtil.getCurrentUserAccountId(), term, term,
+                pageRequest);
 
-    void saveUploadedFiles(List<MultipartFile> files) throws IOException {
-        for (MultipartFile file : files) {
-            saveUploadedFile(file);
-        }
-    }
-
-    void saveUploadedFile(MultipartFile file) throws IOException {
-
-        if (file.isEmpty() || !isImage(file)) {
-            return;
-        }
-
-        byte[] bytes = file.getBytes();
-
-        createDirectory(uploadPath);
-        Path path = Paths.get(String.format("%s/%s", uploadPath, file.getOriginalFilename()));
-        Files.write(path, bytes);
-    }
-
-    Resource loadFileAsResource(String fileName) {
-        try {
-
-            Path path = Paths.get(String.format("%s/%s", uploadPath, fileName));
-            Resource resource = new UrlResource(path.toUri());
-            if (resource.exists()) {
-                return resource;
-            }
-        } catch (MalformedURLException ex) {
-            throw new RuntimeException("File not found " + fileName, ex);
-        }
-        return null;
-    }
-
-
-    private static void createDirectory(String target) {
-        final Path path = Paths.get(target);
-
-        if (Files.exists(path)) {
-            return;
-        }
-
-        try {
-            Files.createDirectories(path);
-        } catch (IOException ioe) {
-            throw new RuntimeException(ioe);
-        }
+        return toPageModels(noteEntityList);
     }
 }
