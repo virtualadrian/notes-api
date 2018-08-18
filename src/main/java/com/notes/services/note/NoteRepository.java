589bb6eedb58b53dd29716b4a8468e9b2f35f8be
Adrian Pavelescu <adrian@acpgr.com>
2018-08-18 13:51:49 -0400

    search, and paging by name and title

--- src/main/java/com/notes/services/note/NoteRepository.java
+++ src/main/java/com/notes/services/note/NoteRepository.java
@@ -1,6 +1,5 @@
 package com.notes.services.note;
 
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +10,4 @@ public interface NoteRepository extends JpaRepository<NoteEntity, Long> {
 
     Page<NoteEntity> findAllByAccountIdAndNoteBodyContainsOrNoteTitleContains(final Long accountId,
         final String searchTerm, final String searchTitle, Pageable pageable);
-
-
-    List<NoteEntity> findAllByNoteBodyContaining();
 }
