82c727b17a24c54756fe1c4acd989247f8d2d7a4
Adrian Pavelescu <adrian@acpgr.com>
2018-08-19 03:37:20 -0400

    rewrote password reset for registration and reset to one

--- src/main/java/com/notes/services/account/AccountService.java
+++ src/main/java/com/notes/services/account/AccountService.java
@@ -15,6 +15,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -55,17 +56,52 @@ public class AccountService extends BaseCrudService<AccountModel, AccountEntity,
         return this.toModel(accountRepository.findByUsername(userName));
     }
 
+    Boolean sendPasswordReset(AccountModel account) throws AccountException {
+        if (!emailExists(account.getEmail())) {
+            throw new AccountException("Email address does not exist: " + account.getEmail());
+        }
+
+        User user = userRepository.findByEmail(account.getEmail());
+        sendPasswordResetMessage(user, false);
+        return true;
+    }
+
     AccountModel createNewDisabledAccount(AccountModel newAccount) throws AccountException {
         if (emailExists(newAccount.getEmail())) {
             throw new AccountException("Email address: " + newAccount.getEmail());
         }
         AccountModel created = create(newAccount);
         User user = createUserForAccount(newAccount);
-        sendPasswordResetMessage(user);
+        sendPasswordResetMessage(user, true);
         return created;
     }
 
-    Boolean updateUserPassword(AccountPasswordModel passwordModel) {
+    private Claims validatePasswordChangeRequest(AccountConfirmationModel confirmation) {
+        if (!SecurityUtil
+            .validatePassword(confirmation.getPassword(), confirmation.getConfirmPassword())) {
+            return null;
+        }
+        return verificationService.getResetTokenClaims(confirmation.getToken());
+    }
+
+    private User updateTokenUserPassword(Claims claims, AccountConfirmationModel confirmation) {
+        User user = userRepository.findByEmail(claims.getSubject());
+        setUserPassword(user, confirmation.getPassword());
+        user.setEnabled(true);
+        userRepository.save(user);
+        return user;
+    }
+
+    Boolean resetUserPassword(AccountConfirmationModel confirmation) {
+        Claims claims = validatePasswordChangeRequest(confirmation);
+        if (claims == null) {
+            return false;
+        }
+        updateTokenUserPassword(claims, confirmation);
+        return true;
+    }
+
+    Boolean updateCurrentUserPassword(AccountPasswordModel passwordModel) {
         if (!SecurityUtil
             .validatePassword(passwordModel.getPassword(), passwordModel.getConfirmPassword())) {
             return false;
@@ -77,23 +113,7 @@ public class AccountService extends BaseCrudService<AccountModel, AccountEntity,
         return true;
     }
 
-    Boolean checkTokenAndEnableUser(AccountConfirmationModel confirmation) {
-        if (!SecurityUtil
-            .validatePassword(confirmation.getPassword(), confirmation.getConfirmPassword())) {
-            return false;
-        }
-        Claims claims = verificationService.getResetTokenClaims(confirmation.getToken());
-        if(claims == null) {
-            return false;
-        }
-        User user = userRepository.findByEmail(claims.getSubject());
-        user.setPassword(confirmation.getPassword());
-        user.setEnabled(true);
-        userRepository.save(user);
-        return true;
-    }
-
-    private void sendPasswordResetMessage(User user) {
+    private void sendPasswordResetMessage(User user, Boolean isRegistering) {
 
         Map<String, Object> mailbag = new HashMap<>();
         mailbag.put("firstName", user.getFirstName());
@@ -104,14 +124,17 @@ public class AccountService extends BaseCrudService<AccountModel, AccountEntity,
         mailbag.put("supportEmail", "info@noteler.com");
         mailbag.put("token", verificationService.getResetJwt(user));
 
+        String template = isRegistering ? "newAccountEmail.html" : "passwordResetEmail.html";
+        String subject = isRegistering ? String.format("Welcome! %s!", user.getFirstName())
+            : "Noteler Password Reset";
+
         mailService.sendTemplatedMessage(user.getEmail(), "system@noteler.com",
-            String.format("Welcome! %s!", user.getFirstName()), "newAccountEmail.html",
-            mailbag);
+            subject, template, mailbag);
     }
 
     private User createUserForAccount(AccountModel newAccount) {
         final User user = new User();
-        String randomPass = DigestUtils.sha256Hex(LocalDateTime.from(Instant.now()).toString());
+        String randomPass = DigestUtils.sha256Hex(Date.from(Instant.now()).toString());
         setUserPassword(user, randomPass);
         user.setUsername(newAccount.getUsername());
         user.setFirstName(newAccount.getFirstName());
